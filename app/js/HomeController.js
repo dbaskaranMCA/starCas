27cc6e1acefbd0c0417bb700b8b23ef8b81df19e
Baskaranxiphias <baskaran@xiphiastec.com>
2018-07-10 13:28:07 +0530

    first commit

---
+++ app/js/HomeController.js
@@ -0,0 +1,62 @@
+﻿
+var helmetcontroller = angular.module("starCasController", []);
+helmetcontroller.controller('HomeController', ['$scope', function ($scope) {
+
+    Highcharts.chart('container', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: false
+        },
+        credits: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Kiosk',
+                y: 56.33
+            }, {
+                name: 'Web',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Booking.com',
+                y: 10.38
+            }, {
+                name: 'Android App',
+                y: 4.77
+            }, {
+                name: 'IOS App',
+                y: 0.91
+            }, {
+                name: 'Other',
+                y: 0.2
+            }]
+        }]
+    });
+
+
+}]);
---
+++ app/js/appConfig.js
@@ -0,0 +1,26 @@
+﻿var helmet = angular.module("starCas", ["ngRoute", "starCasController"]);
+
+helmet.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "views/home.html", controller: "HomeController" })
+
+        .when("/addATM", { templateUrl: "views/addATM.html", })
+        .when("/editATM", { templateUrl: "views/editATM.html", })
+        .when("/viewATM", { templateUrl: "views/viewATM.html", })
+        .when("/deleteATM", { templateUrl: "views/deleteATM.html", })
+        .when("/addBranch", { templateUrl: "views/addBranch.html", })
+        .when("/editBranch", { templateUrl: "views/addBranch.html", })
+        .when("/viewBranch", { templateUrl: "views/addBranch.html", })
+        .when("/deleteBranch", { templateUrl: "views/addBranch.html", })
+        .when("/editBranch", { templateUrl: "views/editBranch.html", })
+        .when("/ATMtype", { templateUrl: "views/ATMtype.html", })
+        .when("/institution", { templateUrl: "views/institution.html", })
+        .when("/networkMonitor", { templateUrl: "views/networkMonitering.html", })
+        .when("/hsmMonitor", { templateUrl: "views/hsmMonitor.html", })
+        .when("/txnMonitor", { templateUrl: "views/txnMonitor.html", })
+        .when("/About", { templateUrl: "templates/aboutView.html", })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
