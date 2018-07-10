b709e258cf19f40e609b414e4461470960756d81
Baskaranxiphias <baskaran@xiphiastec.com>
2018-07-10 14:09:03 +0530

    update

--- app/js/appConfig.js
+++ app/js/appConfig.js
@@ -2,7 +2,7 @@
 
 helmet.config(["$routeProvider", function ($routeProvider) {
     $routeProvider
-        .when("/", { templateUrl: "views/home.html", controller: "HomeController" })
+        .when("/home", { templateUrl: "views/home.html", controller: "HomeController" })
 
         .when("/addATM", { templateUrl: "views/addATM.html", })
         .when("/editATM", { templateUrl: "views/editATM.html", })
@@ -18,9 +18,10 @@ helmet.config(["$routeProvider", function ($routeProvider) {
         .when("/networkMonitor", { templateUrl: "views/networkMonitering.html", })
         .when("/hsmMonitor", { templateUrl: "views/hsmMonitor.html", })
         .when("/txnMonitor", { templateUrl: "views/txnMonitor.html", })
+        .when("/login", { templateUrl: "login.html", })
         .when("/About", { templateUrl: "templates/aboutView.html", })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/home'
         });
 }]);
 
