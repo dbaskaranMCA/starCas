b6639dc982654d316d0a672f4b7850a7b9af7111
Baskaranxiphias <baskaran@xiphiastec.com>
2018-07-12 09:53:39 +0530

    update

--- app/js/HomeController.js
+++ app/js/HomeController.js
@@ -1,6 +1,30 @@
 ﻿
 var helmetcontroller = angular.module("starCasController", []);
-helmetcontroller.controller('HomeController', ['$scope', function ($scope) {
+helmetcontroller.controller('atmController', ['$scope', function ($scope) {
+
+
+    $('#atmTable').DataTable({
+
+
+        dom: 'frtlip',
+        language: {
+            "searchPlaceholder": "search",
+            "search": '<i class="icon icon-search search-icon-align"></i>',
+          
+            "paginate": {
+                "previous": '<i class="icon icon-angle-double-left"></i>',
+                "next": '<i class="icon icon-angle-double-right"></i>'
+            },
+            sLengthMenu: "_MENU_"
+
+
+        },
+
+        aoColumnDefs: [{
+            orderable: false, aTargets: [0, -1] //disable sorting for the 1st column
+        }],
+        order: []
+    });
 
     Highcharts.chart('container', {
         chart: {
